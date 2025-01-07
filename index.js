b14ae9d982d16953f254394c8d15655eaa45d3e4
heliostar9k <121747218+heliostar9k@users.noreply.github.com>
2025-01-07 09:19:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.post('/api/search', async (req, res) => {
       }]
     });
 
-    const searchStrategy = JSON.parse(completion.content[0].text);
+    const searchStrategy = JSON.parse(completion.content);
     console.log('Search strategy:', searchStrategy);
 
     // Build base query
